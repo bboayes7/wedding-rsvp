55349b984d0fc2b8ef15ea4d2d007ad158e644fd
Barry <barryboayes17@gmail.com>
2022-08-19 22:03:44 -0700

    changes

--- server/server.js
+++ server/server.js
@@ -4,12 +4,12 @@ const port = process.env.PORT || 5000
 const connectDB = require('./config/db')
 const cors = require('cors')
 const path = require('path')
-app.use(express.static(path.join(__dirname, 'client/build')))
 
 connectDB()
 
 const app = express()
 
+app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
